3015185ba2e952dbdb4e858fe805552452ad3acc
Eric Karlins <karlinser@mail.nih.gov>
2018-04-06 12:53:13 -0400

    updated mainSnake.sh

--- mainSnake.sh
+++ mainSnake.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 module load python/3.5
-cd /data/karlinser/OsteoExome/interVar/working/Test6
-sbcmd="sbatch --export=ALL --cpus-per-task={threads} --mem={cluster.mem}"
+cd $PWD
+sbcmd="sbatch --cpus-per-task={threads} --mem={cluster.mem}"
 sbcmd+=" --time={cluster.time} --partition=quick"
 sbcmd+=" --out={cluster.out} {cluster.extra}"
-snakemake -pr --keep-going --local-cores 1 --jobs 100 --cluster-config cluster.json --cluster "$sbcmd" --latency-wait 120 all
+snakemake -pr --keep-going --local-cores 1 --jobs 6000 --cluster-config cluster.json --cluster "$sbcmd" --latency-wait 120 all
