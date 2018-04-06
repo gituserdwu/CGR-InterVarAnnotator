23029433b9c477c0872cd40ecd33c4aa39596aef
Eric Karlins <karlinser@mail.nih.gov>
2018-04-06 13:33:00 -0400

    added mkdir logs to mainSnake.sh

--- mainSnake.sh
+++ mainSnake.sh
@@ -2,6 +2,7 @@
 
 module load python/3.5
 cd $PWD
+mkdir logs
 sbcmd="sbatch --cpus-per-task={threads} --mem={cluster.mem}"
 sbcmd+=" --time={cluster.time} --partition=quick"
 sbcmd+=" --out={cluster.out} {cluster.extra}"
