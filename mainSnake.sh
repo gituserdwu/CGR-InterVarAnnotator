1158dca23445857efae3820af9fbde7742295d46
Eric Karlins <karlinser@mail.nih.gov>
2018-04-10 20:42:21 -0400

    updated mainSnake.sh to use cluster.partition

--- mainSnake.sh
+++ mainSnake.sh
@@ -4,6 +4,6 @@ module load python/3.5
 cd $PWD
 mkdir logs
 sbcmd="sbatch --cpus-per-task={threads} --mem={cluster.mem}"
-sbcmd+=" --time={cluster.time} --partition=quick"
+sbcmd+=" --time={cluster.time} --partition={cluster.partition}"
 sbcmd+=" --out={cluster.out} {cluster.extra}"
 snakemake -pr --keep-going --local-cores 1 --jobs 6000 --cluster-config cluster.json --cluster "$sbcmd" --latency-wait 120 all
